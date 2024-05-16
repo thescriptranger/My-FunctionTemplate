8a7625eabaccd289ca6d32d716b535a145154eee
The Script Ranger <thescriptranger@gmail.com>
2024-05-16 11:44:12 -0700

    Fifth Commit

---
+++ _function/My-ChildFunction.ps1
@@ -0,0 +1,20 @@
+Function Get-SomethingElse {
+    <#
+    .SYNOPSIS
+        A Child Function
+     
+    .NOTES
+        Name: Get-Something
+        Author: Script Ranger
+        Version: 1.0
+        DateCreated: 2024.01.01
+     
+    .EXAMPLE
+        Get-SomethingElse
+     
+    .LINK
+        https://github.com/YourRepositoryLinkHere
+    #>
+    
+
+}
